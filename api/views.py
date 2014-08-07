1cad14a34aac09fef5ee1c09f19c5f1b01d3aece
Drew Wu <drewswu@gmail.com>
2014-08-07 16:00:58 -0700

    Fixed some pylint code style issues

--- api/views.py
+++ api/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from json import dumps, loads
 from moviemap.settings import MAP_API_SOURCE_URL
@@ -11,15 +10,15 @@ def locations(request, city):
   """
   try:
     movies_json = urllib2.urlopen(MAP_API_SOURCE_URL).read()
-  except (urllib2.HTTPError, urllib2.URLError, urllib2.HTTPException) as e:
+  except (urllib2.HTTPError, urllib2.URLError) as e:
     print "Error: Could not execute GET request for %s\n%s" % (MAP_API_SOURCE_URL, e)
   title = request.GET.get('title')
   if title:
     try:
-      locations = loads(movies_json)
+      locations_obj = loads(movies_json)
     except ValueError as e:
       print "Error: Can not decode json: %s\n%s" % (movies_json, e)
-    title_locations = [ location for location in locations if location['title'].lower() == title.lower() ]
+    title_locations = [ location for location in locations_obj if location['title'].lower() == title.lower() ]
     movies_json = dumps(title_locations)
   return HttpResponse(movies_json, content_type="application/json")
 
@@ -29,12 +28,12 @@ def titles(request, city):
   """
   try:
     movies_json = urllib2.urlopen(MAP_API_SOURCE_URL).read()
-  except (urllib2.HTTPError, urllib2.URLError, urllib2.HTTPException) as e:
+  except (urllib2.HTTPError, urllib2.URLError) as e:
     print "Error: Could not execute GET request for %s\n%s" % (MAP_API_SOURCE_URL, e)
   try:
-    locations = loads(movies_json)
+    locations_obj = loads(movies_json)
   except ValueError as e:
     print "Error: Can not decode json: %s\n%s" % (movies_json, e)
-  titles_set = set([ location['title'] for location in locations ])  # De-dupe titles
+  titles_set = set([ location['title'] for location in locations_obj ])  # De-dupe titles
   titles_json = dumps({ 'titles': list(titles_set) })
   return HttpResponse(titles_json, content_type="application/json")
--- webapp/views.py
+++ webapp/views.py
@@ -1,9 +1,11 @@
-#from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from moviemap.settings import CITY_TABLE
 
 def index(request, city):
+  """
+  Render and return the index.html template.
+  """
   context = RequestContext(request)
   context_dict = { 'city': CITY_TABLE[city] }
   return render_to_response('webapp/index.html', context_dict, context)
